081de088d50ae4e8e5e4aff11918877be5ce406f
Student <Student>
2018-03-10 11:30:49 +0300

    functions and arrays

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var student = ["Taspyn", "Xavierah", "Ruth", "Naomi", "Paul", "Joachim", "Thomas"]
+var ShoeSize = [39, 2,  38,39, 40, 41,45]
+
+// get the length of the array
+console.log(student.length) 
+
+console.log(student[3])
+for (i=0;i<=6;i++){
+	console.log(student[i])}
